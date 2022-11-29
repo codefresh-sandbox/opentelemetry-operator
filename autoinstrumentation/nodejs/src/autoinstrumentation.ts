d6d4f64ae9543186a0f2132c9929cd0f26cfb8cd
Ilia Medvedev <ilia.medvedev@codefresh.io>
2022-11-29 09:06:06 +0200

    add graphql

--- autoinstrumentation/nodejs/src/autoinstrumentation.ts
+++ autoinstrumentation/nodejs/src/autoinstrumentation.ts
@@ -3,9 +3,17 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 
 import { NodeSDK } from '@opentelemetry/sdk-node';
 
+const { GraphQLInstrumentation } = require('@opentelemetry/instrumentation-graphql');
+
 const sdk = new NodeSDK({
     autoDetectResources: true,
-    instrumentations: [getNodeAutoInstrumentations()],
+    instrumentations: [getNodeAutoInstrumentations(),
+                        new GraphQLInstrumentation({
+                            // optional params
+                            // allowValues: true,
+                            // depth: 2,
+                            // mergeItems: true,
+                            })],
     traceExporter: new OTLPTraceExporter(),
 });
 
